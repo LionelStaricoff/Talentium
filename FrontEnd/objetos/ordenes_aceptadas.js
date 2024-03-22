de36003afd20e210401d6d9d195ca7af4a782a27
lionel staricoff <staricofflionel@hotmail.com>
2024-03-22 18:40:41 -0500

    agregando el seteo de imagenes a todos los templates

---
+++ FrontEnd/objetos/ordenes_aceptadas.js
@@ -0,0 +1,16 @@
+import {Util} from './Util.js'
+
+//Util.existLogin();
+
+if(Util.reuperarImagen() !== null){
+const imagen = Util.reuperarImagen() ;
+
+const logoUser = document.querySelector('#logoUser');
+logoUser.src = imagen;
+}
+
+const volverbutton = document.querySelector('#volverbutton');
+volverbutton.addEventListener('click', ()=> Util.volverUltimaPagina());
+
+const cerrarSesion = document.querySelector('.cerrar');
+cerrarSesion.addEventListener('click', ()=> Util.borrarSession() );
