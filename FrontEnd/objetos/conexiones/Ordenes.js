35dc8314bb6e53d5e5b5d4c520a125316fc77420
lionel staricoff <staricofflionel@hotmail.com>
2024-03-29 00:34:48 -0500

    aplicando el  filtrado de las ordenes tomadas en el front

--- FrontEnd/objetos/conexiones/Ordenes.js
+++ FrontEnd/objetos/conexiones/Ordenes.js
@@ -143,6 +143,40 @@ console.log(data)
             });
 
     }
+
+    listarTodasLasOrdenesPendientesDelProfesional(padreDto) {
+            const url = `${Util.conexionBase()}/api/order/allbyprofessionalidOrderstatus/${Util.reuperarLogin().id}/Pendiente`;
+            const userData = {
+                cliente_id: this.datos.id,
+                description: this.textarea
+            };
+    
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer${Util.reuperarAuthorization()}`
+                },
+    
+            }).then(response => response.json())
+                .then(data => {
+    
+                    if (data.content.length > 0) {
+                        data.content.forEach(d => {
+                            const nuevaOrden = new TomarOrdenesClientes(d, padreDto);
+                            nuevaOrden.agregarAlFront();
+                        });
+    
+                    } else {
+                        new cartelAviso('no hay ordenes creadas', 'h2');
+                    }
+                }
+                ).catch(err => {
+                    new cartelAviso('Ups!! algo salio mal, intenta más tarde', 'h2');
+    
+                });
+    
+    }
 }
 
 
