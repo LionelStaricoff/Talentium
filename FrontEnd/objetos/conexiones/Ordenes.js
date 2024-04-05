d48ee28534dbc807cbd4530bb9bd3d7effd99d5e
lionel staricoff <staricofflionel@hotmail.com>
2024-04-05 10:27:57 -0500

    agregando el objeto para ordenes finalizadas del clinete

--- FrontEnd/objetos/conexiones/Ordenes.js
+++ FrontEnd/objetos/conexiones/Ordenes.js
@@ -233,7 +233,7 @@ export class Ordenes {
                         console.log(d)
                         if (d.order.orderstatus === "Pendiente") nuevaOrden = new AceptarOrdenesClientes(d, padreDto);
                         if (d.order.orderstatus === "Aprobada") nuevaOrden = new AceptarOrdenesClientesAprobada(d, padreDto);
-
+                        if (d.order.orderstatus === "Finalizada") nuevaOrden = new OrdenesClientesFinalizada(d, padreDto);
                         nuevaOrden.agregarAlFront();
                     });
 
@@ -770,6 +770,141 @@ class AceptarOrdenesClientesAprobada {
     }
 }
 
+class OrdenesClientesFinalizada {
+    constructor(datoOrden, divPadre) {
+        this.datos = datoOrden.order;
+        this.textarea;
+        this.divPadre = divPadre ?? '#front';
+        this.comentario = datoOrden.order.comentarios ?? "";
+        this.description = datoOrden.order.description;
+        this.precio;
+    }
+
+    crearOrden() {
+        const div = document.createElement('div');
+        div.classList.add("CrearOrden");
+
+        this.textarea = document.createElement('textarea');
+        this.textarea.name = "descripcion";
+        this.textarea.placeholder = 'comentario';
+        this.textarea.cols = "30";
+        this.textarea.rows = "8";
+        this.textarea.readOnly = true;
+        this.textarea.value = this.datos.description_professional;
+
+
+        this.precio = document.createElement('input');
+        this.precio.type = "text";
+        this.precio.readOnly = true;
+        this.precio.value = '$ ' + this.datos.price;
+
+        const button = document.createElement('button');
+        button.innerText = this.datos.orderstatus;
+        button.addEventListener('click', () => { 
+                new cartelAviso('Orden finalizada', 'h2');
+        });
+
+      
+
+        const buttonX = document.createElement('button');
+        buttonX.innerText = "X";
+        buttonX.addEventListener('click', () => {
+            div.classList.remove('aceptarOrden');
+            div.classList.add("CrearOrden");
+        });
+
+        const descripcionOrden = document.createElement('div');
+        descripcionOrden.innerText = this.description;
+        descripcionOrden.addEventListener('click', () => {
+            div.classList.remove('CrearOrden');
+            div.classList.add("aceptarOrden");
+        });
+
+        div.append(buttonX, this.textarea, this.precio, button, descripcionOrden);
+        return div;
+    }
+    agregarAlFront() {
+        const main = document.querySelector(this.divPadre);
+        main.appendChild(this.crearOrden());
+    }
+    tomarOrden() {
+        const url = `${Util.conexionBase()}/api/order/${this.datos.id}`;
+        const userData = {
+            idProfessional: Util.reuperarLogin().id,
+            descriptionProfessional: this.textarea.value,
+            price: this.precio.value,
+
+        };
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer${Util.reuperarAuthorization()}`
+            },
+            body: JSON.stringify(userData)
+        }).then(response => response.json())
+            .then(data => {
+                new cartelAviso('Orden tomada', 'h2');
+
+            }
+            ).catch(err => {
+                new cartelAviso('Ups!! algo salio mal, intenta más tarde', 'h2');
+
+            });
+
+
+    }
+
+    ordenAceptarPresupuesto() {
+        const url = `${Util.conexionBase()}/api/order/acepted/${this.datos.id}`;
+
+
+        fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer${Util.reuperarAuthorization()}`
+            },
+
+        }).then(response => response.json())
+            .then(data => {
+                new cartelAviso('Presupuest aceptado', 'h2');
+
+            }
+            ).catch(err => {
+                new cartelAviso('Ups!! algo salio mal, intenta más tarde', 'h2');
+
+            });
+
+
+    }
+
+    ordenCancelar() {
+        const url = `${Util.conexionBase()}/api/order/cancelorder/${this.datos.id}`;
+
+
+        fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer${Util.reuperarAuthorization()}`
+            },
+
+        }).then(response => response.json())
+            .then(data => {
+                new cartelAviso('Presupuest aceptado', 'h2');
+
+            }
+            ).catch(err => {
+                new cartelAviso('Ups!! algo salio mal, intenta más tarde', 'h2');
+
+            });
+
+
+    }
+}
+
 class profesionalOrdenesPendienes {
     constructor(datoOrden, divPadre) {
         this.datos = datoOrden.order;
@@ -932,3 +1067,4 @@ class profesionalOrdenesPendienes {
 
    
 }
+
